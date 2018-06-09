ce6d1c58e798a99f794aea526f4c64b332555c5c
Zen <zn_aen_aen@yahoo.co.jp>
2018-06-09 11:01:25 +0900

    add

---
+++ js/script.js
@@ -0,0 +1,13 @@
+$("body").html();
+
+$(function(){
+    $("body").append("<img src='mystudio.jpg' class='mystudio' alt'mystudio'");
+    $("img.mystudio").css({
+        "position":"fixed",
+        "bottom":"10",
+        "right":"0",
+        "left":"0",
+        "margin":"auto",
+    })
+    $("body").append("<img src='return.jpg'")
+})
