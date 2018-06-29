bdb33f25bd43f5065739d72f260ad148c6525559
Zen-logi <zn_aen_aen@yahoo.co,jp>
2018-06-29 23:57:04 +0900

    Okay
    kjn

--- js/script.js
+++ js/script.js
@@ -1,40 +1,41 @@
+<script type="text/javascript" src="jquery.ztwitterfeed.min2.js" charset="uft-8"></script>
 $("body").html();
 
-$(function(){
+$(function () {
     $("body").append("<img src='mystudio.jpg' class='mystudio' alt='mystudio'>");
     $("img.mystudio").css({
-        "position":"fixed",
-        "bottom":"10",
-        "right":"0",
-        "left":"0",
-        "margin":"auto",
-    })
-    $("body").append("<img src='img/return.jpg' class='return' alt='return'>")
+        "position": "fixed",
+        "bottom": "10",
+        "right": "0",
+        "left": "0",
+        "margin": "auto",
+    });
+    $("body").append("<img src='img/return.jpg' class='return' alt='return'>");
     $("img.return").css({
-        "position":"fixed",
-        "bottom":"10",
-        "left":"5",
-        "margin":"auto"
-    })
-    $("body").append("<img src='img/next.jpg' class='next' alt='next'>")
+        "position": "fixed",
+        "bottom": "10",
+        "left": "5",
+        "margin": "auto"
+    });
+    $("body").append("<img src='img/next.jpg' class='next' alt='next'>");
     $("img.next").css([
         "position": "fixed",
         "botton": "10",
         "left": "15",
         "margin": "auto"
-    ])
-    $("body").append("<img src='img/menu.jpg' class='manu' alt='menu'>")
+    ]);
+    $("body").append("<img src='img/menu.jpg' class='manu' alt='menu'>");
     $("img.menu").css({
         "position": "fized",
         "bottom": "10",
         "right": "5",
         "margin": "auto"
-    })
-    $("body").append("<img src='img/reload.jpg' class='reload' alt='reload'>")
+    });
+    $("body").append("<img src='img/reload.jpg' class='reload' alt='reload'>");
     $("img.reload").css({
         "position": "fixed",
         "botton": "10",
         "right": "10",
-        "margin":"auto"
-    })
-})
+        "margin": "auto"
+    });
+});
